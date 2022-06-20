852019978ab475c8105a6334c20ef510daece23d
agasigabrielyan <agasi79@list.ru>
2022-06-20 13:46:11 +0300

    Создать компонент со стандартными возможностями

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
+    const [formData, setFormData] = React.useState({
+        login: "",
+        password: "",
+        remember: true
+    });
+
+    function onChange(event) {
+        const {name, value, type, checked} = event.target;
+
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name] : type === 'checkbox' ? checked : value
+            }
+        })
+    }
+
+    function onSubmit(event) {
+        debugger;
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+          <h2>Авторизация</h2>
+          <label for="authorization-login">
+                Логин
+                <input onChange={onChange} value={formData.login} placeholder="Логин" id="authorization-login" type='text' name="login" />
+          </label>
+          <label for="authorization-password">
+              Пароль
+              <input onChange={onChange} value={formData.password} placeholder="Пароль" id="authorization-password" type="password" name="password" />
+          </label>
+          <label for="authorization-remember">
+              <input onChange={onChange} checked={formData.remember} id="authorization-remember" type="checkbox" name="remember" />
+              <span>Запомнить меня</span>
+          </label>
+          <button>
+              Войти
+          </button>
+      </form>
     </div>
   );
 }
